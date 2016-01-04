4f14f2c05cb2408c472742fc22b1dbc7eaa6e82d
root <root@training3.training3>
2016-01-03 23:07:23 -0800

    Update raadme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-tg-bot
+BOT
+=================
+
+- 使用前環境準備
+    - 準備一台有 domain 並且公開的 web server
+    - 建立 Telegram bot
+    - 產生 SSL key, 不需要經過認證, 但是 "Common Name" 必須同於伺服器名稱
+
+### set web-hook example
+```sh
+    curl -F url="https://bot.xxx.com/secret/web-hook.php" \
+         -F certificate="@/private/ssl-key/telegram/ca.crt" \
+         https://api.telegram.org/bot???:??????/setWebhook
+```
--- app/Api/Setting.php
+++ app/Api/Setting.php
@@ -15,19 +15,24 @@ class Setting extends BaseApi
         $status = attrib('status');
         switch (strtolower($status)) {
             case 'on':
-                // TODO: 未測試!!
+                put('根據測試, 必須夾帶 crt 檔案才能正確設定 web hook, 請手動設定.');
+                /*
                 $telegram = new \Telegram\Bot\Api(conf('bot.token'));
                 $response = $telegram->setWebhook([
                     'url' => conf('bot.hook_file')
                 ]);
                 put( $response );
-                exit;
+                */
+                return;
 
             case 'off':
+                put('目前不提供該 關閉 web hook 的指令');
+                /*
                 $telegram = new \Telegram\Bot\Api(conf('bot.token'));
                 $response = $telegram->removeWebhook();
                 put( $response->getDecodedBody() );
-                exit;
+                */
+                return;
 
             default:
                 break;
