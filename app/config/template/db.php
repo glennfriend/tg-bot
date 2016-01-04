eda7f91de60e17f030fdeecc8535763e55f1b57f
root <root@training3.training3>
2016-01-03 23:59:32 -0800

    Add db config template

---
+++ app/config/template/db.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /**
+     *  mySQL Adapter
+     */
+    'mysql' => [
+        'host' => 'localhost',
+        'user' => 'root',
+        'pass' => '',
+        'db'   => 'telegram_bot',
+    ],
+
+    /**
+     *  每頁顯示多少筆資料 預設值
+     */
+    'items_per_page' => 15,
+
+];
